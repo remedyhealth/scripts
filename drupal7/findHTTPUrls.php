991011d652cc25ec3429ad182206ed4cba7eacd7
Julka Grodel <jgrodel@remedyhealthmedia.com>
2019-05-06 16:05:20 -0400

    feat: Adds scripts for checking if a URL is good, finding HTTP urls in drupal content, updating HTTP urls to HTTP in drupal content [TPV-4]

---
+++ drupal7/findHTTPUrls.php
@@ -0,0 +1,154 @@
+<?php
+require_once('utilities.php');
+
+$host = askQuestion('database host');
+$password = askQuestion('database port');
+$username = askQuestion('database username');
+$password = askQuestion('database password');
+$database = askQuestion('database name');
+
+$connection = mysqli_connect($host, $username, $password, $database, $port);
+
+// a query for retrieving all the relevant tables & columns in the database
+$queryRelevantTablesAndColumns = "
+    SELECT c.TABLE_NAME, c.COLUMN_NAME, c.COLUMN_TYPE
+        FROM INFORMATION_SCHEMA.COLUMNS c
+        WHERE TABLE_NAME IN
+        (
+            SELECT TABLE_NAME
+                FROM INFORMATION_SCHEMA.TABLES
+                WHERE TABLE_TYPE = 'BASE TABLE'
+                AND TABLE_SCHEMA='bw'
+                AND TABLE_NAME != 'BW_POSSIBLE_LOCATIONS'
+        )
+        AND (
+            c.COLUMN_TYPE LIKE '%varchar%' OR
+            c.COLUMN_TYPE LIKE '%text%' OR
+            c.COLUMN_TYPE LIKE '%blob%' OR
+            c.COLUMN_TYPE LIKE '%char%'
+        )
+";
+
+// a query that looks in a provided table & column and uses a regex to search
+// for http
+// 1 table
+// 2 column
+$queryFindHttp = '
+  SELECT %2$s FROM %1$s WHERE %2$s RLIKE \'[[:<:]]http[[:>:]]\'
+';
+
+echo "\n";
+// a variable to hold a list of tables that contain no http matches
+$noMatches = [];
+// a variable to hold the list of columns to migrate, added by interaction
+// with the command line user
+$migrate = [];
+// a variable to hold the list of columns not to migrate, added by interaction
+// with the command line user
+$blacklist = [];
+// a variable to hold the list of all URLs found in db
+$allHttpUrls = [];
+
+// ask questions about what to output as we go
+$showContent = askYesNoQuestion('Show content?');
+$showAllContent = $showContent ? askYesNoQuestion('Show all content?') : false;
+$showHttpUrl = askYesNoQuestion('Show HTTP URLS in real time?');
+$showHttpsUrl = askYesNoQuestion('Show HTTPs URLS in real time?');
+$showUrlsUnavailableOverHttps = askYesNoQuestion('Show URLs unavailable over HTTPS in real time?');
+$markForMigration = askYesNoQuestion('Mark columns for migration as you go?');
+
+$loopThruResultsForEachColumn = $showContent || $showHttpUrl || $showHttpsUrl || $showUrlsUnavailableOverHttps;
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: {${mysqli_connect_error()}}";
+} else {
+  // get a list of relevant columns
+  if ($relevantTablesAndColumns = $connection->query($queryRelevantTablesAndColumns)) {
+    echo "==================================================================\n";
+    echo "= Found {$relevantTablesAndColumns->num_rows} matching database columns\n";
+    echo "==================================================================\n";
+
+    // for each column
+    while ($relevantColumn = $relevantTablesAndColumns->fetch_assoc()) {
+        $table = $relevantColumn['TABLE_NAME'];
+        $column = $relevantColumn['COLUMN_NAME'];
+        $tableColumn = $table . "." . $column;
+
+        if (in_array($tableColumn, $blacklist) || isset($migrate[$tableColumn])) {
+            // if this column is in the hardcoded list of ones to skip during
+            // migrating, continue on to the next result
+            continue;
+        }
+
+        // find all entires that have http in them with word boundries around it
+        $foundHttp = $connection->query(sprintf($queryFindHttp, $table, $column));
+
+        if ($foundHttp->num_rows) {
+            echo "== Found {$foundHttp->num_rows} matches rows in {$table}.{$column}\n";
+            echo "==================================================================\n";
+
+            while ($loopThruResultsForEachColumn && ($stringWithHttp = $foundHttp->fetch_assoc())) {
+                $content = $stringWithHttp[$column];
+                if ($showContent) {
+                    echo "=== Content from {$tableColumn}\n";
+                    echo "==================================================================\n";
+                    echo $content;
+                    echo "==================================================================\n";
+                }
+                $matchUrls = findHttpUrlsInString($content);
+                foreach ($matchUrls as $httpUrl) {
+                    if ($showHttpUrl) {
+                        echo "=== HTTP URL: {$httpUrl}\n";
+                    }
+                    $httpsUrl = replacementUrl($httpUrl);
+
+                    if ($showHttpsUrl && $httpsUrl) {
+                        echo "=== HTTPS URL {$httpsUrl}\n";
+                    }
+
+                    if ($showUrlsUnavailableOverHttps && !$httpsUrl) {
+                        echo "=== HTTP URL not available over HTTPS: {$httpUrl}\n";
+                    }
+                }
+
+                $allHttpUrls = array_merge($allHttpUrls, $matchUrls);
+                $showAnother = $showAllContent ? true : askYesNoQuestion('Show another?');
+                if (!$showAnother) {
+                    break;
+                }
+            }
+            // ask user what to do with this table?
+            if ($markForMigration) {
+                echo "==================================================================\n";
+                if (askYesNoQuestion('Mark column for migration?')) {
+                    array_push($migrate, [$table, $column]);
+                } else {
+                    array_push($blacklist, "{$tableColumn}");
+                }
+                echo "==================================================================\n";
+            }
+        } else {
+            array_push($noMatches, "{$table}.{$column}");
+        }
+
+        // $foundHttp->close();
+    }
+    $relevantTablesAndColumns->close();
+  }
+}
+$connection->close();
+
+// ask questions about what results to output (& output those results when told)
+askQuestionPrintArray("Show columns without matches?", $noMatches);
+if ($markForMigration) {
+    askQuestionPrintArray("Show columns marked for migration?", $migration);
+    askQuestionPrintArray("Show columns marked not to migratate", $blacklist);
+}
+
+if ($loopThruResultsForEachColumn) {
+    askQuestionPrintArray("Show all URLs?", $allHttpUrls);
+    askQuestionPrintArray("Show all URLs unavailable over HTTPS?", getAllUrlsUnavailableOverHttps());
+    askQuestionPrintArray("Show all URLs available over HTTPS?", getAllMappedUrls());
+}
+
+echo "\n";
---
+++ drupal7/updateHTTPUrlsToHTTPS.php
@@ -0,0 +1,150 @@
+<?php
+require_once('utilities.php');
+
+$host = askQuestion('database host');
+$password = askQuestion('database port');
+$username = askQuestion('database username');
+$password = askQuestion('database password');
+$database = askQuestion('database name');
+
+$connection = mysqli_connect($host, $username, $password, $database, $port);
+
+// a query that looks in a provided table & column and uses a regex to search
+// for http
+// 1 table
+// 2 column to be set
+// 3 columns used to identify line uniquely
+$queryFindHttp = '
+  SELECT %2$s, %3$s FROM %1$s WHERE %2$s RLIKE \'[[:<:]]http[[:>:]]\'
+';
+
+// 1 table
+// 2 set
+// 3 content
+// 4 where
+$updateContent = '
+  UPDATE bw.%1$s SET %2$s = \'%3$s\' WHERE %4$s
+';
+
+echo "\n";
+
+// tables and columns to migrate and how to identify unique rows in them
+$migrations = [
+    "block_custom" => ["set" => "body", "where" => ["bid"]],
+    "field_data_body" => ["set" => "body_value", "where" => ["revision_id"]],
+    "field_data_field_body_link_intel" => ["set" => "field_body_link_intel_value", "where" => ["revision_id"]],
+    "field_data_field_description" => ["set" => "field_description_value", "where" => ["revision_id"]],
+    "field_data_field_note_description" => ["set" => "field_note_description_value", "where" => ["revision_id"]],
+    "field_data_field_photo_source" => ["set" => "field_photo_source_value", "where" => ["revision_id"]],
+    "field_data_field_photo_url" => ["set" => "field_photo_url_value", "where" => ["revision_id"]],
+    "field_data_field_redirect_url" => ["set" => "field_redirect_url_value", "where" => ["field_redirect_url_value"]],
+    "field_data_field_slide_photo_source" => ["set" => "field_slide_photo_source_value", "where" => ["revision_id"]],
+    "field_data_field_slide_photo_url" => ["set" => "field_slide_photo_url_value", "where" => ["revision_id"]],
+    "field_data_field_social_image" => ["set" => "field_social_image_value", "where" => ["revision_id"]],
+    "link_keywords" => ["set" => "keyword_url", "where" => ["kid"]],
+    "menu_links" => ["set" => "link_path", "where" => ["mlid"]],
+    "profile_value" => ["set" => "value", "where" => ["fid", "uid"]],
+    "redirect" => ["set" => "redirect", "where" => ["rid"]],
+    "variable" => ["set" => "value", "where" => ["name"]], // serialized
+    "views_display" => ["set" => "display_options", "where" => ["vid", "id"]], // serialized, but also has PHP and html
+    // "watchdog" => ["set" => "location", "where" => "wid"],
+    // "watchdog" => ["set" => "referer", "where" => "wid"]
+];
+
+// special serialized columns
+$serialized = [
+    "variable.value" => true,
+    "views_display.display_options" => true,
+];
+
+// ask questions about what to output as we go
+$isDryRun = !askYesNoQuestion('Migrate instead of doing a dry run?');
+$updateCount = 0;
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: {${mysqli_connect_error()}}";
+} else {
+    foreach ($migrations as $table => $columns) {
+        $select = sprintf(
+            $queryFindHttp,
+            $table,
+            $columns['set'],
+            implode(', ', $columns['where'])
+        );
+        $foundHttp = $connection->query($select);
+        if ($foundHttp->num_rows) {
+            echo "== Found {$foundHttp->num_rows} matches rows in {$table}.{$columns["set"]} {$type} \n";
+            echo "==================================================================\n";
+            $rowCount = 1;
+            while ($stringWithHttp = $foundHttp->fetch_assoc()) {
+                $content = $stringWithHttp[$columns['set']];
+
+                if (array_key_exists("{$table}.{$columns["set"]}", $serialized)) {
+                    echo "this is serialized";
+                    $unserializedContent = unserialize($content);
+                    if (is_string($unserializedContent)) {
+                        $migratedContent = replaceHttpForHttpsInContent($unserializedContent);
+                    } else if (is_array($unserializedContent)) {
+                        // loop over the values, and by reference so updates to
+                        // the variable created for the loop ($value1 & $value2)
+                        // update the corresponding value in $unserializedContent
+                        foreach ($unserializedContent as $key1 => &$value1) {
+                            if(is_array($value1)) {
+                                foreach ($value1 as $key2 => &$value2) {
+                                    if (preg_match('/^php/', $key2) &&
+                                        array_key_exists("php_output", $value2) &&
+                                        is_string($value2["php_output"])
+                                    ) {
+                                        // if key2 is like php, php_1, php2, run migration over content
+                                        $value2["php_output"] = replaceHttpForHttpsInContent($value2["php_output"]);
+                                    }
+                                }
+                            }
+                        }
+                        $migratedContent = $unserializedContent;
+                    }
+                    $migratedContent = serialize($migratedContent);
+                } else {
+                    $migratedContent = replaceHttpForHttpsInContent($content);
+                }
+                echo "{$rowCount} of {$foundHttp->num_rows} ";
+                if ($migratedContent !== $content) {
+                    $whereConditionals = [];
+                    foreach ($columns['where'] as $column) {
+                        $escapedValue = mysqli_real_escape_string($connection, $stringWithHttp[$column]);
+                        $conditional = "{$column} = '{$escapedValue}'";
+                        array_push($whereConditionals, $conditional);
+                    }
+                    $where = implode(" AND ", $whereConditionals);
+                    $escapedContent = mysqli_real_escape_string($connection, $migratedContent);
+                    $update = sprintf($updateContent, $table, $columns['set'], $escapedContent, $where);
+                    if ($isDryRun) {
+                        // echo out the update statement
+                        echo $update;
+                    } else {
+                        // run the update statement
+                        if ($connection->query($update) === TRUE) {
+                            echo "Record updated successfully";
+                            $updateCount++;
+                        } else {
+                            echo "Error updating record: {$connection->error}";
+                        }
+                    }
+                } else {
+                    echo "Nothing to update";
+                }
+                echo "\n";
+                $rowCount++;
+            }
+        }
+    }
+}
+
+$connection->close();
+
+echo "{$updateCount} records updated\n";
+// ask questions about what results to output (& output those results when told)
+askQuestionPrintArray("Show all URLs unavailable over HTTPS?", getAllUrlsUnavailableOverHttps());
+askQuestionPrintArray("Show all URLs available over HTTPS?", getAllMappedUrls());
+
+echo "\n";
