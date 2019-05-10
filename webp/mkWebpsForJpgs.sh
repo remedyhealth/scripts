9a40ea2ef8f91448da72e038406ab4398445b3a6
Sav Samoylov <ssamoylov@remedyhealthmedia.com>
2019-05-10 10:02:34 -0400

    adjustments

--- webp/mkWebpsForJpgs.sh
+++ webp/mkWebpsForJpgs.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
-files=$(find ./ -name '*.jpg')
-for jpg in $files
-do
-  webp=${jpg/%jpg/webp}
-  if [ ! -f $webp ]; then
-    echo "The webp version does not exist";
-  fi
+# To install Google's webp conversion library using Homebrew.
+# run `brew install webp`
+# You should now be able to use the `cwebp` command.
+#
+find ./ -type f -name  "*.jpg" |
+  while read jpg
+  do
+    webp="${jpg/%jpg/webp}";
+    if [ ! -f "$webp" ]; then
+      echo "The webp version does not exist";
+      cwebp -q 80 "$jpg" -o "$webp";
+    fi
 done
 
-files=$(find ./ -name '*.jpeg')
-for jpg in $files
-do
-  webp=${jpg/%jpeg/webp}
-  if [ ! -f $webp ]; then
-    echo "The webp version does not exist";
-  fi
+find ./ -type f -name  "*.jpeg" |
+  while read jpeg
+  do
+    webp="${jpeg/%jpeg/webp}";
+    if [ ! -f "$webp" ]; then
+      echo "The webp version does not exist";
+      cwebp -q 80 "$jpeg" -o "$webp";
+    fi
 done
 
-files=$(find ./ -name '*.png')
-for png in $files
-do
-  webp=${png/%png/webp}
-  if [ ! -f $webp ]; then
-    echo "The webp version does not exist";
-  fi
+find ./ -type f -name  "*.png" |
+  while read png
+  do
+    webp="${png/%png/webp}";
+    if [ ! -f "$webp" ]; then
+      echo "The webp version does not exist";
+      cwebp -q 80 "$png" -o "$webp";
+    fi
 done
