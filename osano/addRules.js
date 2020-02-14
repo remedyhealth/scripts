80d222e7a840bc416787dae8279711e57f3433c3
Julka Grodel <jgrodel@remedyhealthmedia.com>
2020-02-14 10:41:48 -0500

    feat: add in customer id and all config ids for Osano [MS-1399]

---
+++ osano/addRules.js
@@ -0,0 +1,14 @@
+const customerId = 'AzyWD9Rj3Kr4TrGb'
+const siteIds = {
+  staging: '0f7030d4-cde7-4fac-adb4-f376cde894bd',
+  // bw: '358a621b-b0a9-432d-822b-1312e55411b7', // BerkeleyWellness.com
+  // corporate: '31297f70-72d8-4c9d-9f23-d7b626c296b7', // RemedyHealthMedia.com
+  // ew: 'AzyWD9Rj3Kr4TrGb/32c7b394-d21c-4138-a2e2-64f8f186b736', // EndocrineWeb.com
+  // spu: '0b647056-4cf8-451e-bb6c-95a3b85a1ad5', // SpineUniverse.com
+  // tb: '1c1c81d9-7469-44d3-bc79-7f7e600879ba', // TheBody.com
+  // tbp: '19313a59-aad4-4ad9-a516-6667bad0d8dd', // TheBodyPro.com
+  // hc: '488f608b-3d91-4858-945b-17b38eb77158', // HealthCentral.com
+  // ppm: '87d5744a-d884-42fe-befd-d44a6bbf2684', // PracticalPainManagement.com
+  // psycom: '267cf210-8836-4f4d-ade7-aa7e0b816562' // PsyCom.net
+}
+
